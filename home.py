3db0074dd96fa1e35fe2460c7afb398c8befcf69
Pere-coder <gpere800@gmail.com>
2024-11-15 17:12:41 +0100

    edits

--- home.py
+++ home.py
@@ -9,13 +9,19 @@ app = Flask(__name__)
 def do_the_login():
     return '<p>logged in<p>'
 
+def valid_login(username, password):
+    if username == 'pere' and password =='1234':
+        return username , password
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error=None
     if request.method == 'POST':
-        return do_the_login()
-    else:
-        return do_the_login()
-
+        if valid_login(request.form['username'], request.form['password']):
+            return render_template('hello.html', name = request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    return render_template('hello.html', error=error)
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
